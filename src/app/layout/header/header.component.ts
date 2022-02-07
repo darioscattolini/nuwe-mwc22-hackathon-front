4ecc8610acbd7b7dc3186df63d799227f8b0effe
Dario Scattolini <dario.scattolini@gmail.com>
2022-02-07 19:43:05 +0100

    add UserService and retrieve user logged in status from that service

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/models/user';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'mwc-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
-  public isUserLogged?: User;
+export class HeaderComponent implements OnInit {
+  public loggedUser?: User;
 
-  constructor() { }
+  constructor(
+    private userService: UserService
+  ) { }
 
+  public ngOnInit(): void {
+    if (this.userService.loggedUser) {
+      this.loggedUser = this.userService.loggedUser;
+    }
+  }
 }
