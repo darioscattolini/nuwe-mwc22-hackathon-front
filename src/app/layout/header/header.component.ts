9e0ecb8e8e4c7fe944dae617d7fd5c75e9d17daf
Dario Scattolini <dario.scattolini@gmail.com>
2022-02-07 19:26:48 +0100

    make header actions conditional on user logged in

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'mwc-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
+  public isUserLogged?: User;
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
